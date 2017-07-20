0e7ea75b57d867f5ccbd45f9d79c6af68e2d8908
chibat <tomofumi.chiba@gmail.com>
2017-07-20 23:23:24 +0900

    first commit

---
+++ src/main/java/app/appname/AppnameApplication.java
@@ -0,0 +1,12 @@
+package app.appname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppnameApplication.class, args);
+	}
+}
